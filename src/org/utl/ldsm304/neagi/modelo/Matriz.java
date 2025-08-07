4a6b2e4211e035141099053c959119f1575b6d27
DenisseMaxson <denijoshler@gmail.com>
2025-08-07 10:09:01 -0600

    Soy denisse y cree agregué la matriz bidimensional y getter en la clase matriz

--- src/org/utl/ldsm304/neagi/modelo/Matriz.java
+++ src/org/utl/ldsm304/neagi/modelo/Matriz.java
@@ -9,5 +9,14 @@ package org.utl.ldsm304.neagi.modelo;
  * @author Deni
  */
 public class Matriz {
-    
+     private int[][] datos = {
+        {50, 120, 50, 80},
+        {75, 100, 120, 90},
+        {50, 65, 75, 120},
+        {80, 100, 80, 50}
+    };
+     
+    public int[][] getDatos() {
+        return datos;
+    }
 }
